19613b84d5b7a83cf8b0c3d8c135ad0ced45652e
AmadeusGB <1069830494@qq.com>
2020-05-31 21:40:56 +0800

    Update lib.rs

--- demo/runtime/src/lib.rs
+++ demo/runtime/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2017 Parity Technologies (UK) Ltd.
 // This file is part of Substrate Demo.
+// This is a Test
 
 // Substrate Demo is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
